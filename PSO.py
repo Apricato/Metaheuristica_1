0062b6b3744257efbce10c996c013869973f1d15
jgallegos1 <jgallegos@griddynamics.com>
2024-09-22 20:44:41 -0600

    [QoL] update refs: score -> average_load

--- PSO.py
+++ PSO.py
@@ -1,11 +1,15 @@
 # Create a Particle Swarm Optimization-based algorithm that distributes the loads from 100,000 tasks each taking 1 second, to 8 processors. The algorithm should minimize the total time taken to complete all tasks. The algorithm should be implemented in Python and should be able to run on a single machine with 8 processors
 from random import randint, random
 
+# Base parameters
 particles = 100
 processors = 8
 tasks = 100000
+batches = 10
 task_time = 1
-max_iterations = 1000
+preprocess_time = 0.0
+postprocess_time = 0.0
+max_iterations = 0
 c1 = 2
 c2 = 2
 w = 0.9
@@ -15,7 +19,7 @@ class Particle:
         self.position = position
         self.velocity = velocity
         self.best_position = position
-        self.best_score = float('inf')
+        self.best_average_load = float('inf')
     
     def update_position(self):
         self.position = [max(0, min(p, tasks)) for p in self.position]
@@ -23,16 +27,16 @@ class Particle:
     def update_velocity(self, global_best_position):
         self.velocity = [w*v + c1*random()*(bp-p) + c2*random()*(global_best_position[i]-p) for i, (p, v, bp) in enumerate(zip(self.position, self.velocity, self.best_position))]
     
-    def update_best(self, score):
-        if score < self.best_score:
+    def update_best(self, average_load):
+        if average_load < self.best_average_load:
             self.best_position = self.position
-            self.best_score = score
+            self.best_average_load = score
     
     def log_load(self):
         print(f'Load: {sum(self.position)}')
     
     def __str__(self):
-        return f'Position: {self.position}, Best Position: {self.best_position}, Best Score: {self.best_score}'
+        return f'Position: {self.position}, Best Position: {self.best_position}, Best Score: {self.best_average_load}'
     
     def __repr__(self):
         return str(self)
@@ -41,17 +45,17 @@ class PSO:
     def __init__(self):
         self.particles = [Particle([randint(0, tasks) for _ in range(processors)], [0 for _ in range(processors)]) for _ in range(particles)]
         self.global_best_position = self.particles[0].position
-        self.global_best_score = float('inf')
+        self.global_best_avg_load = float('inf')
     
     def run(self):
         previous_task_number = tasks
         for _ in range(max_iterations):
             for particle in self.particles:
-                score = self.evaluate(particle)
+                avg_load = self.evaluate(particle)
                 particle.update_best(score)
-                if score < self.global_best_score:
+                if avg_load < self.global_best_avg_load:
                     self.global_best_position = particle.position
-                    self.global_best_score = score
+                    self.global_best_avg_load = avg_load
                 particle.update_velocity(self.global_best_position)
                 particle.update_position()
                 current_task_number = sum(particle.position)
@@ -63,7 +67,7 @@ class PSO:
         return sum([p*task_time for p in particle.position])
     
     def __str__(self):
-        return f'Global Best Position: {self.global_best_position}, Global Best Score: {self.global_best_score}'
+        return f'Global Best Position: {self.global_best_position}, Global Best Score: {self.global_best_avg_load}'
     
     def __repr__(self):
         return str(self)
