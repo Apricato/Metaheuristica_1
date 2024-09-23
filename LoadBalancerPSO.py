491ac7ba724ea6a4e4473995612a81edb6e75921
jgallegos1 <jgallegos@griddynamics.com>
2024-09-23 03:00:40 -0600

    [QoL] Add comments to code

--- LoadBalancerPSO.py
+++ LoadBalancerPSO.py
@@ -1,8 +1,9 @@
 import random
 
 # PSO parameters
-NUM_PARTICLES = 1000
+NUM_PARTICLES = 12
 NUM_PROCESSORS = 8
+# PROCESSOR_WEIGHTS = [1, 1, 1, 1, 1, 1, 1, 1]
 TOTAL_TASKS = 100000
 TASKS_PER_INTERVAL = 10000
 CURRENT_TASKS_FOR_LOGGING = 10000
@@ -11,6 +12,8 @@ COGNITIVE_FACTOR = 2
 SOCIAL_FACTOR = 2
 INERTIA_WEIGHT = 0.9
 
+# We create a particle class to represent an individual way of assigning tasks to processors
+# Each particle has a random initial position, and develops a velocity that will help it move towards the best solution
 class Particle:
     def __init__(self):
         # Initialize positions (tasks assigned to processors)
@@ -34,6 +37,7 @@ class Particle:
         tasks = [int(round(f * total_tasks)) for f in fractions]
         
         # Adjust to ensure the sum equals total_tasks
+        # For each processor determine if the difference is positive or negative, then add or remove tasks based on it
         diff = total_tasks - sum(tasks)
         while diff != 0:
             for i in range(num_processors):
@@ -48,12 +52,16 @@ class Particle:
         print(f'Initial tasks {tasks}')
         return tasks
 
+    # Fitness is just how far away the current load is from the mean load
     def evaluate_fitness(self, position):
         # Fitness is the variance of tasks assigned to processors based on the mean load and their current load
         mean_load = sum(position) / len(position)
+        # I wanted to add variance for considering error margins in the load balancing but it wasn't working :(
         # variance = (sum((x - mean_load) ** 2 for x in position) / len(position)) ** 0.5
         return mean_load 
 
+    # Update the velocity of the particle based on the best local and global fitness
+    # We use the regular formula weight * velocity + cognitive * r1 * (best_local - position) + social * r2 * (best_global - position)
     def update_velocity(self, global_best_position):
         mean_load = sum(self.position) / len(self.position)
         load_balance_factor = 0.5  # Adjust this factor as needed
@@ -66,13 +74,17 @@ class Particle:
             social = SOCIAL_FACTOR * r2 * (best_global_fitness - self.position[i])
             # Additional term to balance the load towards mean_load
             load_balance = load_balance_factor * (mean_load - self.position[i])
+            # At this point, velocity can be very large or very small, and the sign represents the direction to change (add or substract)
             self.velocity[i] = INERTIA_WEIGHT * self.velocity[i] + cognitive + social + load_balance
 
+    # The position means the size of the load for each processor
     def update_position(self):
         # Approximate the number of tasks to be added to each processor based on their velocity
         weight_factor = TOTAL_TASKS - sum(self.tasks_processed)
+        # Sometimes this can be zero, so we need to avoid division by zero
         if weight_factor == 0:
             weight_factor = 1
+        # We create weights based on the velocity of each processor
         velocity_weights = [self.velocity[i] / weight_factor for i in range(NUM_PROCESSORS)]
         weights_sum = sum([abs(w) for w in velocity_weights])
         velocity_weights = [w / weights_sum for w in velocity_weights]
@@ -89,6 +101,8 @@ class Particle:
         self.position = [int(round(p)) for p in self.position]
         self.adjust_position()
 
+    # Adjust the position by removing or adding tasks when the weights create a surplus or a deficit
+    # This is done by adding or removing tasks from a random processor until the surplus is exhausted
     def adjust_position(self):
         total_diff = TOTAL_TASKS - sum(self.tasks_processed)
         surplus_tasks = total_diff - sum(self.position)
@@ -105,6 +119,7 @@ class Particle:
                 surplus_tasks += -self.position[i]
                 self.position[i] = 0
 
+    # Simulate a task being processed, removing one from each of the processors
     def process_tasks(self):
         self.tasks_processed = [t + 1 if p > 0 else t for t, p in zip(self.tasks_processed, self.position)]
         self.position = [p - 1 for p in self.position]
